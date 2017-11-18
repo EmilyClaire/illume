b1e53eabf234bb417dd5988f52e2dd7dd2407146
Emily Claire Gok Lucas <eclucas@calpoly.edu>
2017-11-18 00:31:35 -0800

    Playing with nodes

--- chooseScript.js
+++ chooseScript.js
@@ -1,8 +1,29 @@
 window.onload = function() {
     var user = firebase.auth().currentUser;
+    var demographic = null;
+    var companion = null;
+    var userName = null;
     
-    var userName = 
     if (user) {
+        userName = user.G;
+        var query = firebase.database().ref("chat").orderByKey();
+        query.once("value")
+        .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+      // key will be "ada" the first time and "alan" the second time
+      var key = childSnapshot.key;
+        var childData = childSnapshot.val();
+    
+            if(childData.username !== userName){
+                companion = childData.username;
+                demographic = childData.demographics;
+            }
+  });
+});
+    
+    console.log(companion + " " + demographic);
+    }
+    else {
         
     }
 }
